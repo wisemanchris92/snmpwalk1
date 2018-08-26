a8f3fb58592615d6bf2f483af3b0bddfdf65ba02
Chris Wiseman <wisemanchris92@gmail.com>
2018-08-26 14:38:09 -0400

    Update cmd_prmt.java

--- src/cmd_prmt.java
+++ src/cmd_prmt.java
@@ -5,7 +5,8 @@ public class cmd_prmt {
 		// TODO Auto-generated method stub
 		
 		try {
-			Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"dir && ping 192.168.204.20 && snmpwalk -v2c -c I$ilonpublic 192.168.204.20\"");
+			Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"dir && " 
+				+ "ping x.x.x.x && snmpwalk -v2c -c [community string] x.x.x.x \"");
 		}
 		catch (Exception e) {
 			System.out.println("wrong");
